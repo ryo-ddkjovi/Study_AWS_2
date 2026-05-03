9a571967a7a5b1fa23b4c55d319111e5e4f946b9
ryo-ddkjovi <ryo20040604@gmail.com>
2026-05-03 22:29:30 +0900

    構築２S3共有用

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "ryo-terraform-state-20260430"
+    key    = "study-aws-2/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
 provider "aws" {
   region = "ap-northeast-1"
 }
